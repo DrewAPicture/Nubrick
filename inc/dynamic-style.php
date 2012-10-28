2efe0334da1c6d12053fd315ce6d626ac8679db3
Drew Jaynes <info@drewapicture.com>
2012-10-28 16:37:20 -0600

    Include other new files for Nubrick.

---
+++ inc/dynamic-style.php
@@ -0,0 +1,95 @@
+<?php
+header( 'Content-type: text/css');
+
+/**
+ * Provides dynamic styling for:
+ * 	1. Gradient effects in the Header.
+ * 	2. The single post view
+ *  3. Dynamic site width
+ *
+ * @package WordPress
+ * @subpackage Nubrick
+ * @since Nubrick 1.0
+ */
+
+	/**
+	 * Setup Header Colors:
+	 *  1. Get and set header color values.
+	 *  2. Set fallback of transparent if necessary.
+	 *  3. Build reusable linear gradient string.
+	 */
+	$first = isset( $_GET['first'] ) ? '#' . $_GET['first'] : '';
+	$second = isset( $_GET['second'] ) ? '#' . $_GET['second'] : '';
+	$dir = isset( $_GET['dir'] ) ? $_GET['dir'] : '';
+
+	if ( ! $first )
+		$color = $second;
+	elseif ( ! $second )
+		$color = $first;
+	else
+		$color = 'transparent';
+
+	$gradient = sprintf( '%1$s, %2$s, %3$s', $dir, $first, $second );
+
+	/**
+	 * Setup Custom Header Image:
+	 *  1. Handle a custom header image if set.
+	 *  2. Build background declaration with header image value.
+	 */
+	$header_image = isset( $_GET['img'] ) ? urldecode( $_GET['img'] ) : '';
+
+	if ( $header_image )
+		$header_image = 'background: url(' . $header_image . ') no-repeat;' . "\n";
+
+	/**
+	 * Setup Site Width
+	 *  1. Check if width parameter is set, if not, set default of 740.
+	 *  2. Calculate the rem width per the theme's formula in style.css.
+	 */
+	$width = ! empty( $_GET['width'] ) ? $_GET['width'] : 740;
+	$rem_width = $width / 14;
+
+	/**
+	 * Setup single page flag
+	 *
+	 * Set $single to true or false based on passed value.
+	 */
+	$single = ! empty( $_GET['single'] ) ? true : false;
+
+?>
+/* Dynamic Site Width */
+
+@media screen and (min-width: 600px) {
+	.site {
+		max-width: <?php echo $width; ?>px;
+		max-width: <?php echo $rem_width; ?>rem;
+	}
+}
+
+/* Header Gradient */
+.site-header {
+	background-color: <?php echo $color; ?>;
+	background: -webkit-gradient( linear, 0% 0%, 0% 100%, from(<?php echo $first; ?>), to(<?php echo $second; ?>) );
+	background: -webkit-linear-gradient( <?php echo $gradient; ?> );
+	background: -moz-linear-gradient( <?php echo $gradient; ?> );
+	background: -ms-linear-gradient( <?php echo $gradient; ?> );
+	background: -o-linear-gradient( <?php echo $gradient; ?> );
+	filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=<?php echo $first; ?>, endColorstr=<?php echo $second; ?>); /* IE7 */
+	zoom: 1;
+	-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=<?php echo $first; ?>, endColorstr=<?php echo $second; ?>)"; /* IE8 */
+	<?php echo $header_image; ?>
+	background-size: auto auto;
+}
+
+<?php if ( $single ) : ?>
+	
+/* Single Post Overrides */
+#main {
+/*	padding-left: 0;*/
+}
+.site-content {
+/*	float: none;
+	margin: 1.71429rem auto 0;*/
+}
+
+<?php endif; // is_single ?>
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Nubrick ===
+Contributors: Drew Jaynes (DrewAPicture)
+Tags: twentytwelve, child-theme, old-school, blog, personal, kubrick, post-formats, responsive, custom-header, custom-background, two-columns
+Requires at least: 3.4
+Tested up to: 3.5
+Stable tag: 1.0
+
+Description: Nubrick is a child-theme of Twenty Twelve, one of the default bundled WordPress themes.
+
+
+== Description ==
+
+Nubrick works to provide a responsive, updated and comparable version of the old Kubrick or Default theme that shipped with WordPress until version 2.9.2. Headlining features include custom header gradient controls in the Customize screen, simplicity of form and function.
+
+
+== Installation ==
+
+This theme is a child of the Twenty Twelve theme, which must also be installed.
+
+Manual Installation:
+1. Upload the `nubrick` folder to your `/wp-content/themes/` directory.
+
+Installation via the WordPress Dashboard:
+1. In your Dashboard, navigate to the Appearance > Themes menu.
+2. Click the 'Install Themes' tab located in the upper left of the screen.
+3. Type `Nubrick` into the search input and click 'Search'.
+4. Find the theme entitled Nubrick, click the link labeled 'Install Now'.
+
+Activation and use:
+1. Once you've installed the theme, navigate back to the Appearance > Themes menu in your Dashboard.
+2. Find the 'Nubrick' theme and click either the 'Live Preview' or 'Activate' links.
+
+
+== License ==
+Unless otherwise specified, all the theme files, scripts and images
+are licensed under GNU General Public Licemse version 2.
+
+
+== Theme Notes ==
+
+
+
+== Frequently Asked Questions ==
+
+= I can't find the Theme Options =
+Nubrick uses the built-in Customize functionality introduced in WP 3.4. You can access it by navigating to the Appearance > Themes menu and clicking the 'Customize' link next to the Nubrick theme listing.
+
+= I can't get any of my menus to show up in the header =
+By default, Nubrick doesn't support a menu in the header in an attempt to stay true to the design of Kubrick, which Nubrick is based on. You can however load a menu through adding a Custom Menu sidebar widget.
+
+= Why on Earth would you want to revive something so awful as the Default/Kubrick theme?!
+Sometimes you just have to take it back to the basics. Many people continue to use the old, decrepit monster that the Default theme is. Nubrick allows you to use an old-skool design that's up to modern Web standards. Oh and there's cool custom header options even Default/Kubrick didn't have!
+
+== Support ==
+
+Support will be provided via the WordPress.org support forums or via the theme's repository support pages.
+
+= 1.0 =
+* Nubrick's initial relase
+
+
