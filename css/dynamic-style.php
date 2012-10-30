7c0ecf995b2142dea819bdc97e30032e22c10ddb
Drew Jaynes <info@drewapicture.com>
2012-10-30 01:58:18 -0600

    Various tweaks to dynamic-style.php.

--- css/dynamic-style.php
+++ css/dynamic-style.php
@@ -2,14 +2,26 @@
 header( 'Content-type: text/css');
 
 /**
+ * Nubrick Dynamic Stylesheet.
+ *
  * Provides dynamic styling for:
- * 	1. Gradient effects in the Header.
- * 	2. The single post view
- *  3. Dynamic site width
+ *  1. Dynamic site width
+ * 	2. Gradient effects in the Header.
+ *  3. Custom header image
+ *  4. Link color overrides
  *
  * @package Nubrick
  * @since Nubrick 1.0
  */
+
+	
+	/**
+	 * Setup Site Width
+	 *  1. Check if width parameter is set, if not, set default of 860.
+	 *  2. Calculate the rem width per the theme's $rembase formula in style.css.
+	 */
+	$width = ! empty( $_GET['width'] ) ? intval( $_GET['width'] ) : 860;
+	$rem_width = $width / 14;
 
 	/**
 	 * Setup Header Colors:
@@ -21,10 +33,10 @@ header( 'Content-type: text/css');
 	$second = isset( $_GET['second'] ) ? '#' . $_GET['second'] : '';
 	$dir = isset( $_GET['dir'] ) ? $_GET['dir'] : '';
 
-	if ( ! $first )
-		$color = $second;
-	elseif ( ! $second )
+	if ( $first != '' )
 		$color = $first;
+	elseif ( $first == '' && $second != '' )
+		$color = $second;
 	else
 		$color = 'transparent';
 
@@ -41,28 +53,13 @@ header( 'Content-type: text/css');
 		$header_image = 'background: url(' . $header_image . ') no-repeat;' . "\n";
 
 	/**
-	 * Setup global link colors
+	 * Setup link colors
 	 */
 	$links = isset( $_GET['links'] ) ? '#' . $_GET['links'] : '';
 	$hover = isset( $_GET['hover'] ) ? '#' . $_GET['hover'] : '';
-	
-	/**
-	 * Setup Site Width
-	 *  1. Check if width parameter is set, if not, set default of 740.
-	 *  2. Calculate the rem width per the theme's formula in style.css.
-	 */
-	$width = ! empty( $_GET['width'] ) ? $_GET['width'] : 860;
-	$rem_width = $width / 14;
+	?>
 
-	/**
-	 * Check for global link colors
-	 *  1. Get global link color
-	 *  2. Get global link:hover color
-	 */
-
-?>
 /* Dynamic Site Width */
-
 @media screen and (min-width: 600px) {
 	.site {
 		max-width: <?php echo $width; ?>px;
@@ -85,21 +82,18 @@ header( 'Content-type: text/css');
 	background-size: auto auto;
 }
 
-<?php if ( $links ) : ?>
-	/* Global link colors */
-	.page-links a,
-	.gallery-caption a,
-	.widget-area .widget li a,
-	footer.entry-meta a {
-		color: <?php echo $links; ?>;
-	}
-<?php endif; // $links ?>
+/* Link color */
+.page-links a,
+.gallery-caption a,
+.widget-area .widget li a,
+footer.entry-meta a {
+	color: <?php echo $links; ?>;
+}
 
-<?php if ( $hover ) : ?>
-	.page-links aLhover,
-	.gallery-caption a:hover,
-	.widget-area .widget li a:hover,
-	footer.entry-meta a:hover {
-		color: <?php echo $hover; ?>;
-	}
-<?php endif; // $hover ?>
+/* Link hover color */
+.page-links aLhover,
+.gallery-caption a:hover,
+.widget-area .widget li a:hover,
+footer.entry-meta a:hover {
+	color: <?php echo $hover; ?>;
+}
