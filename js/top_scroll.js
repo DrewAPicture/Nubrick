f8b83bd39453b69e6243627a1eab4680af6bd28a
Drew Jaynes <info@drewapicture.com>
2012-10-30 03:33:01 -0600

    Round-out scroll-to-top button into a circle. Adjust balance of up image to better center it.

--- js/top_scroll.js
+++ js/top_scroll.js
@@ -1,23 +1,23 @@
 jQuery(document).ready(function(){
 
 	// hide #back-top first
-	jQuery("#scroll-top").hide();
+	jQuery( "#scroll-top" ).hide();
 	
 	// fade in #back-top
-	jQuery(function () {
-		jQuery(window).scroll(function () {
-			if (jQuery(this).scrollTop() > 100) {
-				jQuery('#scroll-top').fadeIn();
+	jQuery( function () {
+		jQuery( window ).scroll( function () {
+			if ( jQuery(this).scrollTop() > 100 ) {
+				jQuery( '#scroll-top' ).fadeIn();
 			} else {
-				jQuery('#scroll-top').fadeOut();
+				jQuery( '#scroll-top' ).fadeOut();
 			}
 		});
 
 		// scroll body to 0px on click
-		jQuery('#scroll-top a').click(function () {
-			jQuery('body,html').animate({
+		jQuery( '#scroll-top a' ).click( function () {
+			jQuery( 'body,html' ).animate({
 				scrollTop: 0
-			}, 800);
+			}, 800 );
 			return false;
 		});
 	});
