162a9cd14810407e24b4be98afa29697d72ec585
Drew Jaynes <info@drewapicture.com>
2012-10-30 03:19:53 -0600

    Add id=top to <body> for scroll-to-top script.

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body id="top" <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
